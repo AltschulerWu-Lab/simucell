07395154f7e1520fb73aa7f0cbde7e824daeaee0
Satwik Rajaram <satwik.rajaram@utsouthwestern.edu>
2012-03-10 13:08:35 -0600

    Documentation 1) Cleaned up the Subpopulation class, and dropped un-used variables. Changed the attributes of the methods and propertis, so that the ones used directly by users are visible. Added documentation. 2) Added some sample that I hope to copy verbatim into all shape/marker plugins

--- src/engine/Subpopulation.m
+++ src/engine/Subpopulation.m
@@ -1,23 +1,136 @@
 classdef Subpopulation <handle
-    %SUBPOPULATION   Different sub-populations (cell-types) are described
+    %SUBPOPULATION   Properties of cells in a  specific sub-population (cell-types) are described
     %by this class. It is the primary class that needs to be defined by the
     %user.
     %Subpopulation properties:
-    %
+    %   objects     - a class of type dynamicprops that defines the
+    %   different objects (nucleus, cytoplams).
+    %   markers     - a class of type dynamicprops that defines thes the
+    %   different markers.
+    %   placement   - the chosen placement model (plugin) to place the
+    %   various objects.
+    %   compositing - the chosen scheme to combine marker levels on overlapping objects.
+    %   cell_artifacts - a cell containing a list of operations (plugins
+    %   classes derived from SimuCell_CellArtifact_Operation), used to
+    %   apply artifacts on a cell by cell basis e.g. Focal Plane Effects
     %Subpopulation methods:
-    %
-    %
+    %   Subpopulation   - Create a new Subpopulation 
+    %   add_object      - Add a new object (nucleus, cytoplasm) to the subpopulation
+    %   add_marker      - Add a new marker to the subpopulation
+    %   add_cell_artifact       - Add a new  cell level artifact operation to
+    %   the subpopulation
     %   See also  SimuCell_Model,
     %   Constant_dependant_marker_level_operation,Perlin_texture
-    %   
     %
-    %   Copyright 2012 - S. Rajaram and B. Pavie for Altschuler and Wu Lab  
+    %
+    %   Copyright 2012 - S. Rajaram and B. Pavie for Altschuler and Wu Lab
     properties
         objects
         markers
-        overlap_Matrix
-        cooperativity_Matrix
         placement
+        compositing
+        cell_artifacts
+        
+    end
+    
+    
+    
+    methods
+        function obj=Subpopulation() 
+        %Subpopulation   - Create a new Subpopulation 
+        %
+        %
+            obj.objects=dynamicprops;%Shape_list;
+            obj.markers=dynamicprops;%Marker_list;
+            
+        end
+        
+        function add_object(obj,obj_name)
+        %   add_object      - Add a new object (nucleus, cytoplasm) to the subpopulation
+        %
+        %   add_object(subpop,obj_name) creates a new property of name obj_name (which
+        %   is assumed to be a string) and type SimuCell_Object in the objects property of the subpopulation specified by subpop.
+        %
+        %   See also  Subpopulation,SimuCell_Object
+        %
+        %   Examples:
+        %   subpop=Subpopulation();
+        %   add_object(subpop,'cytoplasm');
+        %   subpop.objects.cytoplasm.model=Elliptical_nucleus_model;
+        %   set(subpop.objects.cytoplasm.model,'radius',40);
+        %
+        %   Copyright 2012 - S. Rajaram and B. Pavie for Altschuler and Wu Lab
+            obj.objects.addprop(obj_name);
+            obj.objects.(obj_name)=SimuCell_Object();
+            %TODO
+            %Get Marker list and add this new object if not exist to all
+            %this marker
+        end
+        
+        function add_marker(obj,marker_name,marker_color)
+        %   add_marker      - Add a new marker to the subpopulation
+        %
+        %   add_marker(subpop, marker_name,marker_color) creates a new property of name marker_name (a string), 
+        %   type Marker and color specified by marker_color (one of the colors enumerated in Colors) in the markers property in the Subpopulation subpop
+        %   
+        %   
+        %   add_marker(subpop, marker_name,marker_color) creates a new property of name marker_name (a string), 
+        %   type Marker which is Red in color in the markers property in the Subpopulation subpop
+        %   
+        %   See also  Subpopulation,SimuCell_Object,Marker
+        %
+        %   Examples: 
+        %   subpop=Subpopulation();
+        %   add_object(subpop,'cytoplasm');
+        %   subpop.objects.cytoplasm.model=Elliptical_nucleus_model;
+        %   add_marker(subpop,'DAPI','Blue');
+        %   op=Constant_marker_level_operation();
+        %   set(op,'mean_level',0.1);
+        %   set(op,'sd_level',0.1);
+        %   subpop.markers.DAPI.cytoplasm.AddOperation(op);
+        %
+        %   Copyright 2012 - S. Rajaram and B. Pavie for Altschuler and Wu Lab
+            obj.markers.addprop(marker_name);
+            if(nargin>2)
+                obj.markers.(marker_name)=Marker(obj.objects,marker_color);
+            else
+                obj.markers.(marker_name)=Marker(obj.objects);
+            end
+        end
+        
+        function add_cell_artifact(obj,operation)
+        %   add_cell_artifact      - Add a new  cell level artifact operation to
+        %   the subpopulation
+        %
+        %   add_cell_artifact(subpop, operation) adds the cell artifact defined
+        %   by operation to the queue of SimuCell_CellArtifact_Operation-s to
+        %   be performed on cells in Subpopulation subpop. 
+        %   
+        %   See also  Subpopulation,SimuCell_CellArtifact_Operation
+        %
+        %   Examples: 
+        %   subpop=Subpopulation();
+        %   add_object(subpop,'cytoplasm');
+        %   add_marker(subpop,'DAPI','Blue');
+        %   op=Constant_marker_level_operation();
+        %   set(op,'mean_level',0.1);
+        %   set(op,'sd_level',0.1);
+        %   subpop.markers.DAPI.cytoplasm.AddOperation(op);
+        %   op=Out_Of_Focus_Cells();
+        %   set(op,'fraction_blurred',0.04);
+        %   set(op,'blur_radius',4);
+        %   add_cell_artifact(subpop,op); 
+        %
+        %   Copyright 2012 - S. Rajaram and B. Pavie for Altschuler and Wu Lab
+            if(isempty(obj.cell_artifacts))
+                obj.cell_artifacts=cell(0);
+            end
+            obj.cell_artifacts{end+1}=operation;
+            
+        end
+    end
+    
+     properties (Hidden,SetAccess=private)
         
         object_dependancy_struct
         object_dependancy_matrix
@@ -31,47 +144,38 @@ classdef Subpopulation <handle
         full_dependancy_matrix
         full_names
         
-        compositing
-        
-        cell_artifacts
-     
-        
     end
     
     
-    methods
-        function obj=Subpopulation()
-            obj.objects=dynamicprops;%Shape_list;
-            obj.markers=dynamicprops;%Marker_list;
-            
-        end
+    methods (Hidden)
+        
         
         
         function [obvious_deletions,all_deletions]=calculate_all_dependancies(obj,to_be_deleted)
-           shape_names=properties(obj.objects);
-           marker_names=properties(obj.markers);
-           
-           obj.full_dependancy_matrix=false(length(shape_names)*(1+length(marker_names))); 
-           obj.full_names=cell(length(shape_names)*(1+length(marker_names)),1);
-           %shapexshape part of the matrix is the same as the
-           %object_dependancy_matrix calculation
-           for i=1:length(shape_names)
-               dependancies=obj.objects.(shape_names{i}).model.prerendered_object_list();
-               obj.object_dependancy_struct.(shape_names{i})=cell(0);
-               for k=1:length(dependancies)
-                   for j=1:length(shape_names)
-                       if(obj.objects.(shape_names{j})==dependancies{k})
-                           obj.object_dependancy_struct.(shape_names{i}){k}=shape_names{j};
-                       end
-                   end
-               end
-               for j=1:length(shape_names)
-                   obj.full_dependancy_matrix(i,j)=...
-                       any(cellfun(@(x) x==obj.objects.(shape_names{j}),dependancies));
-               end
-               obj.full_names{i}=shape_names{i};
-           end
-           
+            shape_names=properties(obj.objects);
+            marker_names=properties(obj.markers);
+            
+            obj.full_dependancy_matrix=false(length(shape_names)*(1+length(marker_names)));
+            obj.full_names=cell(length(shape_names)*(1+length(marker_names)),1);
+            %shapexshape part of the matrix is the same as the
+            %object_dependancy_matrix calculation
+            for i=1:length(shape_names)
+                dependancies=obj.objects.(shape_names{i}).model.prerendered_object_list();
+                obj.object_dependancy_struct.(shape_names{i})=cell(0);
+                for k=1:length(dependancies)
+                    for j=1:length(shape_names)
+                        if(obj.objects.(shape_names{j})==dependancies{k})
+                            obj.object_dependancy_struct.(shape_names{i}){k}=shape_names{j};
+                        end
+                    end
+                end
+                for j=1:length(shape_names)
+                    obj.full_dependancy_matrix(i,j)=...
+                        any(cellfun(@(x) x==obj.objects.(shape_names{j}),dependancies));
+                end
+                obj.full_names{i}=shape_names{i};
+            end
+            
             
             %Calculate list of all marker-shape pairs for given
             %subpopulation. Each will be a list of operations
@@ -92,9 +196,9 @@ classdef Subpopulation <handle
                 end
             end
             
-                 
             
-          
+            
+            
             %Calculate dependancy of marker on shapes
             for marker_shape_counter=1:length(marker_shape_list)
                 
@@ -105,14 +209,14 @@ classdef Subpopulation <handle
                     dependancies=operations{operation_counter}.needed_shape_list;
                     
                     for j=1:length(shape_names)
-                             if(any(cellfun(@(x) x==obj.objects.(shape_names{j}),dependancies)))
-                                obj.full_dependancy_matrix(length(shape_names)+marker_shape_counter,j)=true;
-                             end
+                        if(any(cellfun(@(x) x==obj.objects.(shape_names{j}),dependancies)))
+                            obj.full_dependancy_matrix(length(shape_names)+marker_shape_counter,j)=true;
+                        end
                         
                         
                     end
                 end
-             
+                
             end
             
             %Calculate dependency of marker on marker
@@ -133,10 +237,10 @@ classdef Subpopulation <handle
                         
                     end
                 end
-
+                
             end
             deleted_number=[];
-          
+            
             
             if(isa(to_be_deleted,'SimuCell_Object'))
                 obvious_deletions=cell(length(marker_names)+1,1);
@@ -270,11 +374,11 @@ classdef Subpopulation <handle
                         
                     end
                 end
-%                 draw_order=dependancy_resolution(obj.marker_dependancy_matrix);
-%                 for i=1:length(draw_order)
-%                     obj.marker_draw_order_objects{i}=marker_shape_list{draw_order(i)};
-%                     obj.marker_draw_order_names{i}=marker_shape_list_names{draw_order(i)};
-%                 end
+                %                 draw_order=dependancy_resolution(obj.marker_dependancy_matrix);
+                %                 for i=1:length(draw_order)
+                %                     obj.marker_draw_order_objects{i}=marker_shape_list{draw_order(i)};
+                %                     obj.marker_draw_order_names{i}=marker_shape_list_names{draw_order(i)};
+                %                 end
             end
             draw_order=dependancy_resolution(obj.marker_dependancy_matrix);
             for i=1:length(draw_order)
@@ -283,13 +387,7 @@ classdef Subpopulation <handle
             end
         end
         
-        function add_object(obj,obj_name)
-            obj.objects.addprop(obj_name);
-            obj.objects.(obj_name)=SimuCell_Object();
-            %TODO
-            %Get Marker list and add this new object if not exist to all
-            %this marker
-        end
+        
         
         function delete_shape(obj,shape_name)
             delete(findprop(obj.objects,shape_name));
@@ -299,14 +397,7 @@ classdef Subpopulation <handle
             end
         end
         
-        function add_marker(obj,marker_name,marker_color)
-            obj.markers.addprop(marker_name);
-            if(nargin>2)
-                obj.markers.(marker_name)=Marker(obj.objects,marker_color);
-            else
-                 obj.markers.(marker_name)=Marker(obj.objects);
-            end
-        end
+        
         
         function delete_marker(obj,marker_name)
             delete(findprop(obj.markers,marker_name));
@@ -348,44 +439,9 @@ classdef Subpopulation <handle
             end
         end
         
-        function add_cell_artifact(obj,operation)
-            if(isempty(obj.cell_artifacts))
-                obj.cell_artifacts=cell(0);
-            end
-             obj.cell_artifacts{end+1}=operation;
-%             marker_names=properties(obj.markers);
-%             if(isempty(obj.cell_artifacts))
-%                 obj.cell_artifacts=struct;
-%                 
-%                 for marker_num=1:length(marker_names)
-%                     obj.cell_artifacts.(marker_names{marker_num})=cell(0);
-%                 end
-%             end
-%             
-%             for marker_num=1:length(marker_names)
-%                 if(obj.markers.(marker_names{marker_num})==marker)
-%                     obj.cell_artifacts.(marker_names{marker_num}){end+1}=operation;
-%                     return;
-%                 end
-%             end
-%             error('Marker Not Found in Subpopulation!');
-            
-        end
         
-        %         function obj=AddObject(obj,object)
-        %             obj.Objects{length(obj.Objects)+1}=object;
-        %             obj.Overlap_Matrix=0.9*triu(ones(length(obj.Objects)))+...
-        %                 0.1*tril(ones(length(obj.Objects)));
-        %             %obj.Overlap_Matrix=ones(length(obj.Objects));
-        %             obj.Cooperativity_Matrix=false(length(obj.Objects));
-        %         end
-        %         function obj=AddMarker(obj,marker)
-        %             obj.Markers{length(obj.Markers)+1}=marker;
-        %         end
-        %         function obj=SetPlacement(obj,placement)
-        %             obj.Placement=placement;
-        %         end
-        %
+        
+        
     end
     
 end
--- src/plugins/markers/Constant_marker_level_operation.m
+++ src/plugins/markers/Constant_marker_level_operation.m
@@ -1,6 +1,7 @@
 classdef Constant_marker_level_operation <SimuCell_Marker_Operation
-    %UNTITLED3 Summary of this class goes here
-    %   Detailed explanation goes here
+    %Constant_marker_level_operation : Sets a constant marker level intensity
+    %for all pixels in a cell. This level is sampled from a normal distribution
+    % of specifiable mean and variation
     
     properties
         mean_level
@@ -9,6 +10,12 @@ classdef Constant_marker_level_operation <SimuCell_Marker_Operation
     end
     
     methods
+       
+        %The constructor needs to have the definitions of all the Parameter variables
+        %declared as properties. Initial pre-processing, that one does not wish
+        %to repeat every time the shape is generated can also be performed
+        %here, potentially using a listener to make sure that all the
+        %Parameters have been set (see SLML_nucleus_model)
         function obj=Constant_marker_level_operation()
             obj.mean_level=Parameter('Mean Marker Level',0.5,SimuCell_Class_Type.number,...
                 [0,1],'Mean Marker Level[0-low, 1-High]');
@@ -18,16 +25,38 @@ classdef Constant_marker_level_operation <SimuCell_Marker_Operation
         
         
         
+        % The Apply function applies a marker operation to the input marker intensity to produce the output marker intensity.
+        % The marker instensities are array of the size of the output image. This function is called by the engine
+        % and will always have the same parameters passed to it:
+        % current_marker - The input intensity of the marker in the cell being
+        % rendered. It is a matrix of the size of the final image, with each
+        % element representing the intensity of the marker. 
+        % current_shape_mask - A binary  mask of the same size as the final image marking the shape (nucleus, cytoplasm etc) currently being rendered.
+        % other_cells_mask -  A binary  mask of the same size as the final image marked with 1s at every pixel occupied by one of the other cells, and 0 otherwise.
+        % needed_shapes - a cell array containing the shapes required (as specified in the needed_shape_list). Each shape is described by binary mask of the same size as the final image and the masks are in the same order as specified in prerendered_object_list
+        % needed_markers - a cell array containing the marker intensities required (as specified in the prerendered_marker_list). Each marker is described by an array of the same size as the final image and the arrays are in the same order as specified in prerendered_marker_list
+        % It is up to the person writing a plugin which part of this
+        % information they want to use.
         function result=Apply(obj,current_marker,current_shape_mask,other_cells_mask,needed_shapes,needed_markers)
             chosen_level=obj.mean_level.value+obj.sd_level.value*randn();
             current_marker(current_shape_mask)=max(0,min(1,chosen_level));
             result=current_marker;
         end
         
+        % This function contains the list of markers on specified shape (within the cell being
+        % rendered),   stored as a cell array, that is required to render the present marker using the
+        % Apply function.  The cell elements are of type
+        % Marker_Operation_Queue, and should be passed to the function by a user
+        % through one of the Parameters.
         function pre_list=prerendered_marker_list(obj)
             pre_list=cell(0);
         end
         
+        % This function contains the list of shapes (within the cell being
+        % rendered), specified as a cell array, that is required to render the present shape using the
+        % output_shape function.  The cell elements are of type
+        % SimuCell_Object, and should be passed to the function by a user
+        % through one of the Parameters.
         function pre_list=needed_shape_list(obj)
             pre_list=cell(0);
         end
